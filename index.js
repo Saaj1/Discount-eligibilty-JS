b0905a36f9b1f6bd86cee56304bb39b550351827
saajida mathen <mathensaajida@gmail.com>
2023-05-05 12:17:53 +0200

    discount eligibilty completed

---
+++ index.js
@@ -0,0 +1,23 @@
+function checkEligibility() {
+
+  // Get user input for age and membership status
+  let age = parseInt(document.getElementById("age").value);
+  let isMember = document.getElementById("member").value === "yes";
+
+  // Check eligibility for discount
+  let isEligible = false;
+
+  if (age >= 18 && isMember) {
+    isEligible = true;
+  }
+
+  // Display eligibility message
+  let result = document.getElementById("result");
+  if (isEligible) {
+    result.innerHTML = "Congratulations! You are eligible for a discount.";
+    result.style.color = "#007200";
+  } else {
+    result.innerHTML = "Sorry, you are not eligible for a discount.";
+    result.style.color = "#ff0000";
+  }
+}
